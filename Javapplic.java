7683f023798287c581193c0b28fca8dc9362d18d
Ramsha0016 <91972311+Ramsha0016@users.noreply.github.com>
2022-03-27 12:58:29 +0530

    This file contains java application
    This file contains java application .HW2

---
+++ Javapplic.java
@@ -0,0 +1,16 @@
+package firstSession;
+
+import java.util.Scanner;
+
+public class Javapplic {
+    public static void main(String[] args) {
+        int arr[] = new int[12];
+
+        System.out.println("Some basic of data type .");
+
+String name;
+        Scanner r = new Scanner(System.in);
+        name=r.nextLine();
+        System.out.println(name);
+    }
+}
