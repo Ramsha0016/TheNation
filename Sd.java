73cea1a22f8eebffb51c6f2e887ff2d12d2feba6
Ramsha0016 <91972311+Ramsha0016@users.noreply.github.com>
2022-03-27 13:28:37 +0530

    Add files via upload

---
+++ Sd.java
@@ -0,0 +1,14 @@
+package firstSession;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Sd {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner("System.in");
+                String name = sc.next();
+        System.out.println("new");
+        HashMap<String, Object>
+
+    }
+}
