710cdbf6dc3e4b6ead4657bc8db8b6f7fdbbb6c3
Ramsha0016 <91972311+Ramsha0016@users.noreply.github.com>
2022-03-27 13:07:30 +0530

    This file contains a test pack

---
+++ TestPack.java
@@ -0,0 +1,30 @@
+package firstSession;
+
+public class TestPack {
+    public static void main(String[] args) {
+//Data types
+        int numbers  = 98;// numbers java
+        int ram =4;
+
+
+                String name = "Just me "; //word or sentence.
+                String ch = "A" ;//characters
+
+                double d = 0.845573313; //decimals
+                float f = 3.5F;
+
+                int sum = 10+ 40;
+
+        System.out.println(sum);
+
+        int d = 100;
+        d = d+1;
+                d++ ;
+
+        System.out.println();
+
+
+        System.out.println("Hi!");
+
+    }
+}
