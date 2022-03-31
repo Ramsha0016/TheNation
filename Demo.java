7e09a9b8d6527de8b75605351a45a5aef5440f30
Ramsha0016 <91972311+Ramsha0016@users.noreply.github.com>
2022-03-31 22:24:50 +0530

    this file contains code hw 3

---
+++ Demo.java
@@ -0,0 +1,27 @@
+package firstSession;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String... args ){
+        String str = "race0016 and B";
+        int y=45;
+
+        String ch = "t";
+        char ch = "f";
+        Scanner sc = new Scanner(System.in);
+        int race0016 = sc.nextInt();
+    sum(num)8, d:90;
+    }
+    public static void main(int num , int d){
+        System.out.println(num1+d);
+        System.out.println("coder");
+
+    }
+public static String String (String getName)
+    public static int subtraction( int n , int a ){
+        int sub= n-a;
+        return sub;
+    }
+
+}
