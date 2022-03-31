32bdc4ff6902cc49d4d473a323a6f50ccf055ff5
Ramsha0016 <91972311+Ramsha0016@users.noreply.github.com>
2022-03-31 22:27:22 +0530

    the file contains  code

---
+++ Employee.java
@@ -0,0 +1,19 @@
+package firstSession;
+
+public class Employee {
+  import practice.Helloworld;
+    //encapsulation -> wrapping up your data and functions into a single unit
+    public class employee{
+        private String name;
+//validification
+if(1>0{
+
+        }
+if(name)==null)
+        public void setName(String name){
+            this.name=name;
+        }
+        public String getName;}
+    name;
+}
+}
